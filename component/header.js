10566182e2bce29ea159ba9506c706415b0044af
anthonyapitchou <anthoapicella@hotmail.fr>
2026-01-24 00:50:52 +0100

    I have done the header, with css and html next time, i would work the footer

---
+++ component/header.js
@@ -0,0 +1,25 @@
+export default function header() {
+    return /*HTML*/ `
+<header class="header">
+<div class="header-container">
+
+<div class="logo">
+<h1>StudioHybrid</h1>
+</div>
+
+<div class="search-bar">
+    <input type="text" id="searchInput" placeholder="Search products">
+    </div>
+
+<nav class="nav">
+
+    <a href="about.index.html">About</a>
+    <a href="contact.html">Contact</a>
+    <a href="login.html">Login</a>
+    <a href="Newlook.html">New Look</a>
+</nav>
+</div>
+</header>
+    `;
+
+}
--- post/app.js
+++ post/app.js
@@ -1,11 +1,6 @@
 import header from "../component/header.js";
-import footer from "../component/footer.js";
-import homePage, { displayPosts } from "../page/homePage.js";
-
 
 
 document.getElementById("header").innerHTML = header();
-document.getElementById("footer").innerHTML = footer();
 
-selectPageNumber();
 displayPosts();
