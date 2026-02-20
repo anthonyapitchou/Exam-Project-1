b2e4ded51bb7c5c19cf6634e9843190aedbb7fee
anthony <126164323+anthonyapitchou@users.noreply.github.com>
2026-02-20 22:15:39 +0100

    Refactor import statements for consistency
    Updated import statements to use lowercase for module names.

--- router/routes.js
+++ router/routes.js
@@ -2,10 +2,10 @@
 
 import about from "../pages/about.js";
 import contact from "../pages/contact.js";
-import Ferragamo from "../pages/Ferragamo.js";
+import ferragamo from "../pages/Ferragamo.js";
 import home from "../pages/home.js";
 import login from "../pages/login.js";
-import Lubutelvintage from "../pages/Lubutelvintage.js";
+import lubutelvintage from "../pages/Lubutelvintage.js";
 import martens from "../pages/martens.js";
 import newlook from "../pages/newlook.js";
 import nike from "../pages/nike.js";
