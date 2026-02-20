de78478bb43c17813d6326384de0e8d6bb22d106
anthony <126164323+anthonyapitchou@users.noreply.github.com>
2026-02-20 23:24:27 +0100

    Update routes.js

--- router/routes.js
+++ router/routes.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-
-import about from "../pages/about.js";
-import contact from "../pages/contact.js";
-import ferragamo from "../pages/ferragamo.js";
-=======
-// routes.js - CORRECTION DES MAJUSCULES (Vérifie bien tes fichiers !)
-
 import about from "../pages/about.js";
 import contact from "../pages/contact.js";
 import ferragamo from "../pages/ferragamo.js"; 
->>>>>>> a8e9173c3c7cbefa59ec3a43f966054af4d29fc5
 import home from "../pages/home.js";
 import login from "../pages/login.js";
 import lubutelvintage from "../pages/lubutelvintage.js"; 
